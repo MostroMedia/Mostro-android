efcb8e5d8744f0e8a7a0155bace9377db3a36992
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-02-13 22:15:38 -0500

    Pantalla de Login

--- app/src/main/java/com/mostromedia/mostro/MainActivity.java
+++ app/src/main/java/com/mostromedia/mostro/MainActivity.java
@@ -16,64 +16,13 @@ import com.mostromedia.mostro.Models.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
-    private DatabaseReference db;
-    private User user;
-    private FirebaseAuth auth;
-    private FirebaseAuth.AuthStateListener listener;
-    public String userId = "";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        user = new User();
 
-        user.setName("Camilo");
-        user.setImage(R.drawable.ic_launcher_foreground);
-        user.setDescription("Amante de la electronica");
-        user.setJob("Developer");
-
-        db = FirebaseDatabase.getInstance().getReference();
-        auth = FirebaseAuth.getInstance();
-        listener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(firebaseAuth != null){
-                    userId = getUid();
-                }else{
-                    Log.w("WARN", "algo esta pasando :(");
-                }
-            }
-        };
-
-        btn = findViewById(R.id.button);
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createNewUser();
-            }
-        });
-
-    }
-
-    public void createNewUser()  {
-
-        Toast.makeText(this,"Adding...", Toast.LENGTH_SHORT).show();
-
-        db.child("users").setValue(user);
-        //db.child("users").child(userId).setValue(user);
-
-    }
-
-    public String getUid(){
-
-        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
 
