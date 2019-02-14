95ee8283ea6492f85fe07b301183f3caaec50204
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-02-13 21:34:19 -0500

    Retomando proyecto

--- app/src/main/java/com/mostromedia/mostro/MainActivity.java
+++ app/src/main/java/com/mostromedia/mostro/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         user = new User();
 
         user.setName("Camilo");
